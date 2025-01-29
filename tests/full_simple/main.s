60e7cc84eb8ed17b351c99c4b15f38c8817ba80f
vroncevic <vladimir.roncevic@htecgroup.com>
2025-01-29 18:58:53 +0100

    [gen_arm32asm] Update setup, version, docs

--- tests/full_simple/main.s
+++ tests/full_simple/main.s
@@ -1,7 +1,7 @@
 @
 @ @brief   full_simple
-@ @version 1.0.1
-@ @date    2025-01-28
+@ @version 1.0.2
+@ @date    2025-01-29
 @ @company None, free software to use 2025
 @ @author  Vladimir Roncevic <elektron.ronca@gmail.com>
 @
